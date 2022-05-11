b6d399ef0b75725fee97a575e372fc0a18bbb6d6
spmarino <spmarino@hotmail.com>
2022-05-11 06:00:43 -0300

    desafío sincronizar counter

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,51 +1,41 @@
 import { useState, useEffect } from "react";
 
 const ItemCount = ({ init, stock, onAdd }) => {
+  const [contador, setContador] = useState(init);
+  const [confirmado, setConfirmado] = useState(false);
 
-    const [contador, setContador] = useState(init);
-    const [confirmado, setConfirmado] = useState(false);
+  const aumentarContador = () => {
+    setContador(contador + 1);
+  };
 
+  const descontarContador = () => {
+    setContador(contador - 1);
+  };
 
-    const aumentarContador = () => {
-        setContador(contador + 1);
+  const confirmar = () => {
+    onAdd(contador);
+  };
 
-    }
-
-    const descontarContador = () => {
-        setContador(contador - 1);
-
-    }
-
-    const confirmar = () => {
-        setConfirmado(true);
-
-    }
-
-
-    if (!confirmado) {
-
-
-        return (
-            <div>
-                <p>Unidades : {contador}</p>
-                <button onClick={aumentarContador}>+ </button>
-                <button onClick={descontarContador}> - </button>
-                <button onClick={confirmar}>Confirmar productos</button>
-            </div>
-        )
-
-    } else {
-        return (
-            <div>
-                <p>Unidades : {contador}</p>
-                <button onClick={aumentarContador}>+ </button>
-                <button onClick={descontarContador}> - </button>
-                <button onClick={confirmar}>Confirmar productos</button>
-                <p className="modal">Se confirmaron {contador} los productos</p>
-            </div>
-        )
-
-    }
-}
+  if (!confirmado) {
+    return (
+      <div>
+        <p>Unidades : {contador}</p>
+        <button onClick={aumentarContador}>+ </button>
+        <button onClick={descontarContador}> - </button>
+        <button onClick={confirmar}>Confirmar productos</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Unidades : {contador}</p>
+        <button onClick={aumentarContador}>+ </button>
+        <button onClick={descontarContador}> - </button>
+        <button onClick={confirmar}>Confirmar productos</button>
+        <p className="modal">Se confirmaron {contador} los productos</p>
+      </div>
+    );
+  }
+};
 
 export default ItemCount;
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -3,11 +3,16 @@ import ItemCount from "./ItemCount";
 
 const ItemDetail = ({producto}) => {
 const {nombre, precio, description} = producto
+
+ const onAdd = (contador) => {
+ console.log(contador)
+}
+ 
   return(<>
   <h3>{nombre}</h3>
   <h3>${precio}</h3>
   <h3>Descripcion: {description}</h3>
-  <ItemCount stock={10} init={1} />
+  <ItemCount stock={10} init={1} onAdd={onAdd} />
   </>)
 }
 
